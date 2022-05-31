140c5c01ffa38d3316b50e015515704335e1662c
Ari | Backdev <90847846+hxAri@users.noreply.github.com>
2022-06-01 00:14:50 +0700

    Update and rename ilapis/CC.py to kanashi/module.py

---
+++ kanashi/module.py
@@ -0,0 +1,16 @@
+
+import json
+import logging
+import os
+import requests
+import sys
+
+from os import system
+from re import findall
+from time import sleep
+from random import choice
+from httpagentparser import simple_detect as useragent
+from getpass import getpass
+from datetime import datetime
+from requests import Session, Response
+from threading import Thread
