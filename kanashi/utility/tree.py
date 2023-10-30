a7523cdfc4a035d9c75f1a71b91583065eac9a29
hxAri <hxari@proton.me>
2023-10-31 06:27:52 +0700

    Fixed ambiguous tree structure bug

--- kanashi/utility/tree.py
+++ kanashi/utility/tree.py
@@ -24,268 +24,185 @@
 #
 
 
-from kanashi.utility.common import typedef, typeof
-
-
-#[kanashi.utility.tree.tree( Dict|List|Set|Tuple data, Int indent ]: String
-def tree( data, indent=0 ):
+#[kanashi.utility.tree( Any data, Int indent )]: Str
+def tree( data:any, indent:int=0 ) -> str:
 	
 	"""
-	Convert data into tree structure.
+	Convert data into the tree structure.
 	
-	:params Dict|List|Set|Tuple data
+	:params Any data
 	:params Int indent
 		Indentation prefix
 	
 	:return String
 	"""
 	
-	STR_LINE = "│   "
-	MID_LINE = "├── "
-	END_LINE = "└── "
-	SPC = "\x20" * 4
+	ITP = "\u00b7"
+	STR_LINE = "\u2502\x20\x20\x20"
+	MID_LINE = "\u251c\u2500\u2500\x20"
+	END_LINE = "\u2514\u2500\u2500\x20"
+	SPC_LINE = "\x20\x20\x20\x20"
 	
-	#[loopv( Mixed value, String space )]: String
-	def loopv( value, space ):
-		
-		"""
-		Handle data value.
-		When data value is Dict|List|Set|Tuple
-		loop function will called for handle that.
-		
-		:params Mixed value
-		:params String space
-			Previous space passed
-		
-		:return String
-		"""
-		
-		if  typedef( value, dict ) or \
-			typedef( value, list ) or \
-			typedef( value, set ) or \
-			typedef( value, tuple ):
-			value = loop( value, space )
+	#[kanashi.utility.tree$.builder( Any data, Int indent, Str space )]: Str
+	def builder( data:any, indent:int, space:str ) -> str:
+		if isinstance( data, ( dict, list, set, tuple ) ):
+			return looping( data=data, indent=indent if indent >= 4 else 4, space=space )
 		else:
-			value = "{}{}{}\n".format( space, END_LINE, value )
-		return value
+			return f"{space}{END_LINE}{data}\x0a"
 	
-	#[loop( Dict|List|Set|Tuple data, String space )]: String
-	def loop( data, space ):
-		
-		"""
-		Handle data loop iteration.
-		
-		:params Dict|List|Set|Tuple data
-		:params String space
-			Indentation prefix
-		
-		:return String
-		"""
-		
-		index = 0
+	#[kanashi.utility.tree$.looping( Dict|List[Any]|Set|Tuple data, Int indent, Str space )]: Str
+	def looping( data:dict|list|set|tuple, indent:int, space:str ) -> str:
 		result = ""
-		if length := len( data ):
-			if typedef( data, list ):
-				for value in data:
-					index += 1
-					if index == length:
-						lk = END_LINE
-						la = SPC
-					else:
-						lk = MID_LINE
-						la = STR_LINE
-					result += space
-					result += lk
-					if typedef( value, dict ):
-						if lenv := len( value ):
-							for idx, key in enumerate( value ):
-								if idx == 0:
-									if idx +1 == lenv:
-										result += "+ "
-										result += key
-										result += "\n"
-										if index != length:
-											result += space + STR_LINE
-										else:
-											result += space + SPC
-										result += END_LINE #+ "\n"
-										result += loopv( value[key], space + STR_LINE + SPC )[len( space + STR_LINE + SPC ) + 4:]
-									else:
-										result += "+ "
-										result += key
-										result += "\n"
-										result += space
-										#result += SPC
-										if index == length:
-											result += SPC
-										else:
-											result += STR_LINE
-										result += MID_LINE #+ "\n"
-										result += loopv( value[key], space + STR_LINE * 2 )[len( space + STR_LINE * 2 ) + 4:]
-									#result += "\n"
-								elif index == length:
-									result += space
-									result += SPC
-									if idx +1 == lenv:
-										result += END_LINE
-									else:
-										result += MID_LINE
-									result += key
-									result += "\n"
-									result += space
-									result += SPC
-									if idx +1 == lenv:
-										result += SPC
-										next = SPC
-									else:
-										result += STR_LINE
-										next = STR_LINE
-									if  typedef( value[key], dict ) or \
-										typedef( value[key], list ):
-										result += MID_LINE
-									else:
-										result += END_LINE #+ "\n"
-									result += loopv( value[key], space + SPC + next )[len( space + SPC + next ) + 4:]
-									#result += "\n"
-								elif idx +1 == lenv:
-									result += space
-									result += STR_LINE
-									result += END_LINE
-									result += key
-									result += "\n"
-									result += space
-									result += STR_LINE
-									result += SPC
-									if  typedef( value[key], dict ) or \
-										typedef( value[key], list ):
-										result += MID_LINE
-									else:
-										result += END_LINE #+ "\n"
-									result += loopv( value[key], space + STR_LINE + SPC )[len( space + STR_LINE + SPC ) + 4:]
-									#result += "\n"
-								else:
-									result += space
-									result += STR_LINE
-									result += MID_LINE
-									result += key
-									result += "\n"
-									result += space
-									result += STR_LINE * 2
-									if  typedef( value[key], dict ) or \
-										typedef( value[key], list ):
-										result += MID_LINE
-									else:
-										result += END_LINE #+ "\n"
-									result += loopv( value[key], space + STR_LINE * 2 )[len( space + STR_LINE * 2 ) + 4:]
-									#result += "\n"
+		if isinstance( data, dict ):
+			length = len( data )
+			if length >= 1:
+				keys = list( data.keys() )
+				length -= 1
+				for index in range( len( keys ) ):
+					keyset = keys[index]
+					value = data[keyset]
+					if index != length:
+						result += space
+						result += MID_LINE
+						result += keyset
+						result += "\x0a"
+						if isinstance( value, ( dict, list, set, tuple ) ):
+							result += builder( data=value, indent=indent, space=space + STR_LINE )
 						else:
+							result += space
+							result += STR_LINE
+							result += END_LINE
 							result += str( value )
-							result += "\n"
-					elif typedef( value, list ):
-						if lenv := len( value ):
-							for idx, val in enumerate( value ):
-								if idx == 0:
-									if idx +1 == lenv:
-										result += "+ "
-										#result += str( idx )
-										result += "\n"
-										if index != length:
-											result += space + STR_LINE
-										else:
-											result += space + SPC
-										result += END_LINE + "+\n"
-										result += loopv( val, space + STR_LINE + SPC )#[len( space + STR_LINE + SPC ) + 4:]
-									else:
-										result += "+ "
-										#result += str( idx )
-										result += "\n"
-										result += space
-										#result += SPC
-										result += STR_LINE
-										result += MID_LINE + "+\n"
-										result += loopv( val, space + STR_LINE * 2 )#[len( space + STR_LINE * 2 ) + 4:]
-									#result += "\n"
-								elif index == length:
-									result += space
-									result += SPC
-									if idx +1 == lenv:
-										result += END_LINE
-									else:
-										result += MID_LINE
-									#result += str( idx )
-									result += "+\n"
-									result += space
-									result += SPC
-									if idx +1 == lenv:
-										result += SPC
-										next = SPC
-									else:
-										result += STR_LINE
-										next = STR_LINE
-									if  typedef( val, dict ) or \
-										typedef( val, list ):
-										result += MID_LINE
-									else:
-										result += END_LINE + "+\n"
-									result += loopv( val, space + SPC + next )#[len( space + SPC + next ) + 4:]
-									#result += "\n"
-								elif idx +1 == lenv:
-									result += space
-									result += STR_LINE
-									result += END_LINE
-									if  typedef( val, dict ) or \
-										typedef( val, list ):
-										result += ""#MID_LINE
-										result += "+\n"
-									else:
-										result += END_LINE
-										result += "+\n"
-									result += loopv( val, space + STR_LINE + SPC )#[len( space + STR_LINE + SPC ) + 4:]
-									#result += "\n"
-								else:
-									result += space
-									result += STR_LINE
-									result += MID_LINE
-									#result += str( idx )
-									result += "+\n"
-									result += space
-									result += STR_LINE * 2
-									if  typedef( val, dict ) or \
-										typedef( val, list ):
-										result += MID_LINE
-									else:
-										result += END_LINE + "+\n"
-									result += loopv( val, space + STR_LINE * 2 )#[len( space + STR_LINE * 2 ) + 4:]
-									#result += "\n"
-						else:
-							result += str( value )
-							result += "\n"
+							result += "\x0a"
 					else:
-						result += value
-						result += "\n"
+						result += space
+						result += END_LINE
+						result += keyset
+						result += "\x0a"
+						if isinstance( value, dict ):
+							result += builder( data=value, indent=indent, space=space + SPC_LINE )
+						elif isinstance( value, ( list, set, tuple ) ):
+							#result += builder( data=value, indent=indent, space=space + SPC_LINE )
+							result += looping( data=value, indent=indent, space=space + SPC_LINE )
+							print( value )
+						else:
+							result += space
+							result += SPC_LINE
+							result += END_LINE
+							result += str( value )
+							result += "\x0a"
 			else:
-				for idx, key in enumerate( data ):
-					index += 1
-					value = data[key]
-					if index == length:
-						lk = END_LINE
-						la = SPC
-					else:
-						lk = MID_LINE
-						la = STR_LINE
-					result += space
-					result += lk
-					result += key
-					result += "\n"
-					if index == length:
-						next = SPC
-					else:
-						next = STR_LINE
-					result += loopv( value, space + next )
+				result += space
+				result += END_LINE
+				result += "{}\x0a"
 		else:
-			result += space
-			result += END_LINE
-			result += str( data )
-			result += "\n"
+			length = len( data )
+			if length >= 1:
+				data = list( data )
+				length -= 1
+				for index in range( len( data ) ):
+					value = data[index]
+					if index != length:
+						if isinstance( value, dict ):
+							if value:
+								key = list( value.keys() )[0]
+								val = value.pop( key )
+								result += space
+								result += MID_LINE
+								result += str( key )
+								result += "\x0a"
+								if len( value ) >= 1:
+									if isinstance( val, ( dict, list, set, tuple ) ):
+										result += space
+										result += STR_LINE
+										result += MID_LINE
+										result += "+\x0a"
+										result += builder( data=val, indent=indent, space=space + STR_LINE + STR_LINE )
+									else:
+										result += space
+										result += STR_LINE
+										result += MID_LINE
+										result += str( val )
+										result += "\x0a"
+									result += builder( data=value, indent=indent, space=space + STR_LINE )
+								else:
+									if isinstance( val, ( dict, list, set, tuple ) ):
+										result += space
+										result += STR_LINE
+										result += MID_LINE
+										result += "+\x0a"
+										result += builder( data=val, indent=indent, space=space + STR_LINE + STR_LINE )
+									else:
+										result += space
+										result += STR_LINE
+										result += END_LINE
+										result += str( val )
+										result += "\n"
+							else:
+								result += space
+								result += MID_LINE
+								result += "{}\x0a"
+						elif isinstance( value, ( list, set, tuple ) ):
+							if value:
+								result += "!=list<list>"
+							else:
+								result += space
+								result += MID_LINE
+								result += "[]\x0a"
+						else:
+							result += space
+							result += MID_LINE
+							result += str( value )
+							result += "\x0a"
+					else:
+						if isinstance( value, dict ):
+							if value:
+								key = list( value.keys() )[0]
+								val = value.pop( key )
+								if len( value ) >= 1:
+									result += space
+									result += END_LINE
+									result += str( key )
+									result += "\x0a"
+									if isinstance( val, ( dict, list, set, tuple ) ):
+										result += space
+										result += SPC_LINE
+										result += MID_LINE
+										result += "+\x0a"
+										result += builder( data=val, indent=indent, space=space + SPC_LINE + STR_LINE )
+									else:
+										result += space
+										result += SPC_LINE
+										result += MID_LINE
+										result += str( val )
+										result += "\x0a"
+									result += builder( data=value, indent=indent, space=space + ( "\x20" * indent ) )
+								else:
+									result += space
+									result += END_LINE
+									result += str( key )
+									result += "\x0a"
+							else:
+								result += space
+								result += END_LINE
+								result += "{}\x0a"
+						elif isinstance( value, ( list, set, tuple ) ):
+							if value:
+								result += "==list<list>"
+							else:
+								result += space
+								result += END_LINE
+								result += "[]\x0a"
+						else:
+							result += space
+							result += END_LINE
+							result += str( value )
+							result += "\n"
+			else:
+				result += space
+				result += END_LINE
+				result += "[]\n"
 		return result
-	return "\u00b7\n" + loop( data, "\x20" * indent )
+	
+	return "\x7b\x7d\x0a\x7b\x7d".format( ITP, builder( data=data, indent=indent, space="\x20" * indent ) )
+	
