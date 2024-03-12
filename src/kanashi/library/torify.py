726c6bb1f63de32f9d5cf8c86787ac418e62c562
hxAri <hxari@proton.me>
2024-03-12 20:38:14 +0700

    [CREATED] Initial commit

---
+++ src/kanashi/library/torify.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+#
+# @author Ari Setiawan
+# @create 23.05-2022
+# @github https://github.com/hxAri/Kanashi
+#
+# Kanashī Copyright (c) 2022 - Ari Setiawan <hxari@proton.me>
+# Kanashī Licence under GNU General Public Licence v3
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# Kanashī is not affiliated with or endorsed, endorsed at all by
+# Instagram or any other party, if you use the main account to use this
+# tool we as Coders and Developers are not responsible for anything that
+# happens to that account, use it at your own risk, and this is Strictly
+# not for SPAM.
+#
+
+
+from builtins import int as Int, str as Str
+from socks import ProxyConnectionError
+from stem.control import Controller
+from stem import ControllerError, Signal, SocketError
+from typing import Dict, final
+
+from kanashi.library.stdio import stdout
+from kanashi.request import request as Request
+
+
+@final
+class Torify:
+	
+	@staticmethod
+	def ipInfo( proxies:Dict[Str,Str]=None ) -> Str:
+		try:
+			response = Request( "GET", "https://api.ipify.org/?format=json", proxies=proxies )
+			content = response.json()
+			if "ip" in content and content['ip']:
+				return content['ip']
+		except ProxyConnectionError as e:
+			...
+		return None
+	
+	@staticmethod
+	def reNewTorIp( proxies:Dict[Str,Str], port:Int, password:Str ) -> None:
+		previousIp = Torify.ipInfo()
+		stdout( Torify.reNewTorIp, f"Trying to re-new IP Address address={previousIp}" )
+		try:
+			with Controller.from_port( port=port ) as controller:
+				controller.authenticate( password=password )
+				controller.signal( Signal.NEWNYM )
+		except SocketError as e:
+			stdout( Torify.reNewTorIp, f"Uncaught SocketError: {e}" )
+			stdout( Torify.reNewTorIp, f"Failed to estabilish connection from={previousIp}" )
+			stdout( Torify.reNewTorIp, f"Failed to update Ip Address from={previousIp}" )
+		except ControllerError as e:
+			stdout( Torify.reNewTorIp, f"Uncaught ControllerError: {e}" )
+			stdout( Torify.reNewTorIp, f"Failed sending Signal from={previousIp}" )
+			stdout( Torify.reNewTorIp, f"Failed to update Ip Address from={previousIp}" )
+		currentIp = Torify.ipInfo( proxies=proxies )
+		if currentIp != previousIp:
+			stdout( Torify.reNewTorIp, f"The IP Address has been updated from={currentIp}" )
+		else:
+			stdout( Torify.reNewTorIp, f"The IP Address does not change from={currentIp}" )
+		...
+	
+	...
