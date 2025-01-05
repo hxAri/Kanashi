a86f342015ba739385e62277fa52dfd6f150f9c3
hxari <hxari@proton.me>
2025-01-05 15:22:19 +0700

    [CREATE] Created src/kanashi/graphql/__init__.py

---
+++ src/kanashi/graphql/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+#
+# @author hxAri (hxari)
+# @create 23-12-2024 17:30
+# @github https://github.com/hxAri/Kanashi
+#
+# Kanashi is an Open-Source project for doing various
+# things related to Facebook, e.g Login. Logout, Profile Info,
+# Follow, Unfollow, Media downloader, etc.
+#
+# Kanashi Copyright (c) 2024 - hxAri <hxari@proton.me>
+# Kanashi Licence under GNU General Public Licence v3
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
+from kanashi.graphql.actions import Action
+from kanashi.graphql.schema import Schema
+
+
+__all__ = [
+	"Action",
+	"Schema"
+]
+
